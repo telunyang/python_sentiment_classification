6a92cbd662c1c455c31c4dea33235aabf7631838
DarrenYang <telunyang@gmail.com>
2021-05-30 14:58:24 +0800

    更新: 訓練、預測程式碼，以及訓練與測試資料

---
+++ predict.py
@@ -0,0 +1,91 @@
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
+import pandas as pd
+import logging
+import time, pprint, datetime 
+import torch
+
+'''
+訓練參考連結
+[1] Binary Classification
+https://simpletransformers.ai/docs/binary-classification/
+[2] Pretrained models
+https://huggingface.co/transformers/pretrained_models.html
+'''
+
+def log():
+    '''warning 時輸出 log'''
+    logging.basicConfig(level=logging.INFO)
+    transformers_logger = logging.getLogger("transformers")
+    transformers_logger.setLevel(logging.WARNING)
+
+    print(f"是否使用 GPU: {torch.cuda.is_available()}")
+
+
+def predict():
+    '''計時開始'''
+    tStart = time.time() 
+
+    '''測試資料'''
+    test_csv = pd.read_csv('test.csv')
+
+    '''放置符合測試格式的資料'''
+    test_data = []
+
+    '''資料轉換'''
+    list_dataset = test_csv.values.tolist()
+    for dataset in list_dataset:
+        text = dataset[1]
+        test_data.append(text)
+    
+    '''pre-trained model、batch size 與 epoch'''
+    model_name = 'roberta-large'
+    batch_size = 512
+    epoch = 5
+
+    '''output 資料夾'''
+    output_dir = f"outputs/{model_name}-epoch-5-cls-model/"
+
+    '''自訂參數'''
+    model_args = ClassificationArgs()
+    model_args.train_batch_size = batch_size
+    model_args.num_train_epochs = epoch
+    model_args.overwrite_output_dir = True
+    model_args.reprocess_input_data = True
+    model_args.output_dir = output_dir
+
+    '''迴歸分析才需要設定'''
+    # model_args.num_labels = 1
+    # model_args.regression = True
+
+    '''建立 ClassificationModel'''
+    model = ClassificationModel('roberta', model_name, use_cuda=True, args=model_args)
+
+    '''預測結果'''
+    predictions, raw_outputs = model.predict(test_data)
+
+    '''放置整合預測結果'''
+    list_result = []
+
+    '''整合預測結果'''
+    for index, sentiment in enumerate(predictions):
+        list_result.append([
+            list_dataset[index][0],
+            sentiment
+        ])
+
+    '''將結果存回 excel'''
+    strDataTime = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
+    result_df = pd.DataFrame(list_result)
+    result_df.columns = ["ID", "sentiment"]
+    # result_df.to_excel(f"{strDataTime}.xlsx", sheet_name='訓練結果')
+    result_df.to_csv(f"{strDataTime}.csv", index=False)
+
+    '''計時結束'''
+    tEnd = time.time()
+
+    '''輸出程式執行的時間'''
+    print("It cost %f sec" % (tEnd - tStart))
+
+if __name__ == "__main__":
+    log()
+    predict()
