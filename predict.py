2639983858c1287e1ed68950b374b2271f4c8b68
DarrenYang <telunyang@gmail.com>
2021-06-11 14:50:01 +0800

    batch_size = 4

--- predict.py
+++ predict.py
@@ -31,7 +31,7 @@ def predict():
     '''pre-trained model、batch size 與 epoch'''
     model = 'roberta'
     model_name_main = 'roberta-large'
-    batch_size = 8
+    batch_size = 4
     epoch = 10
 
     '''output 資料夾'''
