03ed4666d3259a2971fa4f566d409370f8a65772
DarrenYang <telunyang@gmail.com>
2021-06-27 01:49:07 +0800

    batch_size = 56

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def train():
     model_name_prefix = ''
     model_name_main = 'roberta-base'
     model_name = model_name_prefix + model_name_main
-    batch_size = 60
+    batch_size = 56
     epoch = 5 
 
     '''output 資料夾'''
