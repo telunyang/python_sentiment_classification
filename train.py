3c92d6d0915a3bcb274c23679ecfc3fa06f9d92e
DarrenYang <telunyang@gmail.com>
2021-07-23 19:49:27 +0800

    更新程式碼

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def train():
     model_name_prefix = ''
     model_name_main = 'roberta-large'
     model_name = model_name_prefix + model_name_main
-    batch_size = 16
+    batch_size = 8
     epoch = 20
 
     '''output 資料夾'''
