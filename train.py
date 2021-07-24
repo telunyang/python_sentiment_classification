7413f5e606df81b746a94f1e2ba029cb22275258
DarrenYang <telunyang@gmail.com>
2021-07-24 19:42:02 +0800

    更新程式

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def train():
     model_name_prefix = 'microsoft/'
     model_name_main = 'deberta-base'
     model_name = model_name_prefix + model_name_main
-    batch_size = 152
+    batch_size = 154
     epoch = 10
 
     '''output 資料夾'''
