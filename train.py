6eda6e9972e5355aa810a13e6212db9dd4e39e01
DarrenYang <telunyang@gmail.com>
2021-07-24 18:11:39 +0800

    更新程式碼

--- train.py
+++ train.py
@@ -120,12 +120,12 @@ def train():
     train_df.columns = ["text", "labels"]
 
     '''pre-trained model、batch size 與 epoch'''
-    model = 'roberta'
-    model_name_prefix = ''
-    model_name_main = 'roberta-base'
+    model = 'deberta'
+    model_name_prefix = 'microsoft/'
+    model_name_main = 'deberta-base'
     model_name = model_name_prefix + model_name_main
     batch_size = 128
-    epoch = 25
+    epoch = 15
 
     '''output 資料夾'''
     output_dir = f"outputs/{model_name_main}-bs-{batch_size}-ep-{epoch}-cls-model/"
