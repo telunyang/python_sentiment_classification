35941abc33379ac4a6d04726a186f883dd63e9ce
DarrenYang <telunyang@gmail.com>
2021-07-25 18:08:11 +0800

    更新程式碼

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train():
     model_name_main = 'deberta-large'
     model_name = model_name_prefix + model_name_main
     batch_size = 46
-    epoch = 10
+    epoch = 5
 
     '''output 資料夾'''
     output_dir = f"outputs/{model_name_main}-bs-{batch_size}-ep-{epoch}-cls-model/"
