1fa78c43b223bc4008d9f1d8fb5a56f2082ebe0c
DarrenYang <telunyang@gmail.com>
2021-07-26 21:27:57 +0800

    更新程式碼

--- predict.py
+++ predict.py
@@ -32,7 +32,7 @@ def predict():
     model_name_main = 'roberta-large'
     model_name = model_name_prefix + model_name_main
     batch_size = 76
-    epoch = 10
+    epoch = 15
 
     '''output 資料夾'''
     output_dir = f"outputs/{model_name_main}-bs-{batch_size}-ep-{epoch}-cls-model/"
