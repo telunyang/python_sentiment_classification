92f8dc5a2e237b98193450be1b6dba5665f4d49b
DarrenYang <telunyang@gmail.com>
2021-07-27 16:59:55 +0800

    更新程式碼

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train():
     model_name_main = 'roberta-base'
     model_name = model_name_prefix + model_name_main
     batch_size = 246
-    epoch = 10
+    epoch = 15
 
     '''output 資料夾'''
     output_dir = f"outputs/{model_name_main}-bs-{batch_size}-ep-{epoch}-cls-model/"
