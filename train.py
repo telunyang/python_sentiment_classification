3cd35c687fe583e78bc1bb97fd9c690d163fcde6
DarrenYang <telunyang@gmail.com>
2022-05-23 11:27:09 +0800

    歸檔(未測試)

--- train.py
+++ train.py
@@ -122,8 +122,8 @@ def train():
     model_name_prefix = 'microsoft/'
     model_name_main = 'deberta-base'
     model_name = model_name_prefix + model_name_main
-    batch_size = 154
-    epoch = 15
+    batch_size = 32
+    epoch = 20
 
     '''output 資料夾'''
     output_dir = f"outputs/{model_name_main}-bs-{batch_size}-ep-{epoch}-cls-model/"
